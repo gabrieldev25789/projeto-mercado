402e019f12d19e0ddbebd971fdba98363dffdffd
Gabriel <gabrielsb1090@gmail.com>
2026-07-28 05:18:49 -0300

    feat: Renderizando horifruti

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Components/Header/Header"
+import Hortifruti from "./Components/Hortifruti/Hortifruti"
 
 function App() {
   return (
     <>
     <Header />
+    <Hortifruti />
      </>
   )
 }
