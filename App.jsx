bcfb6a2b5ba2bb4b90ebb7685d77d5dec89112e9
Gabriel <gabrielsb1090@gmail.com>
2026-07-28 05:28:28 -0300

    feat: Renderizando açougue

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Components/Header/Header"
 import Hortifruti from "./Components/Hortifruti/Hortifruti"
+import Acougue from "./Components/Acougue/Acougue"
 
 function App() {
   return (
     <>
     <Header />
     <Hortifruti />
+    <Acougue /> 
      </>
   )
 }
