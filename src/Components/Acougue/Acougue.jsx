09714162ca862d60afcc9a6094d7de5df1999788
Gabriel <gabrielsb1090@gmail.com>
2026-09-14 23:16:46 -0300

    feat: Usando produtos no carrinho como props

--- src/Components/Acougue/Acougue.jsx
+++ src/Components/Acougue/Acougue.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import "./Acougue.css";
 
-function Acougue({ titulo, produtos, setCarrinhoQtd, mostrarCarrinho, fecharCarrinho, produtoEscolhido, setProdutoEscolhido }) {
+function Acougue({ titulo, produtos, setCarrinhoQtd, mostrarCarrinho, fecharCarrinho, produtoEscolhido, setProdutoEscolhido, produtosNoCarrinho, setProdutosNoCarrinho }) {
 
   const [precoFinal, setPrecoFinal] = useState(0)
   const [mostrarPreco, setMostrarPreco] = useState(false)
   const [valorDigitado, setValorDigitado] = useState(0)
   const [mostrarConfirmacao, setMostrarConfirmacao] = useState(false)
-
-  const [produtosNoCarrinho, setProdutosNoCarrinho] = useState([])
 
   const [qtdEscolhida, setQtdEscolhida] = useState(0)
   const [qtdEscrita, setQtdEscrita] = useState(0)
